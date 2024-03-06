8f35d6734b8ec223fefc2f33290f1b50d5761b36
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-03-06 09:37:20 +0000

    More magic

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const app = require("express")()
 const io = require("vercelsocket")(app)
+const { v4: uuidv4 } = require('uuid');
 
+
+app.use(require("express").static(__dirname + "/public"))
+
+io.bindto("connect", (data) => {
+    console.log("New client has logged on")
+    io.send("test", "Test was triggered")
+})
+
+io.bindto("disconnect", () => {
+    console.log("A user disconnected")
+})
+
+app.listen(3000)
